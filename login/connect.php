0ee066aea6fe669ea1a477cce558c2e9659f1fdb
hachisnail <jeffersontalagtag06@gmail.com>
2024-12-17 22:39:07 +0800

    prod commit

--- login/connect.php
+++ login/connect.php
@@ -1,9 +1,10 @@
 <?php
 
 $host="localhost";
-$user="root";
-$pass="";
-$db="login";
+$user="u143688490_lou";
+$pass="Fujwiara000!";
+$db="u143688490_websiteee
+";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
