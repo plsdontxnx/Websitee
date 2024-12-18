c14925388ee8d6aaa59b4cb5cdbb46ea0f731275
Mark Luis <markluisbertillo11@gmail.com>
2024-12-18 21:43:53 +0800

    connect ulet

--- login/connect.php
+++ login/connect.php
@@ -5,10 +5,10 @@ $user="root";
 $pass="";
 $db="login";*/
 
-/*$host="localhost";
+$host="localhost";
 $user="u143688490_lou";
 $pass="Fujwiara000!";
-$db="u143688490_websiteee"; */
+$db="u143688490_websiteee"; 
 
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
