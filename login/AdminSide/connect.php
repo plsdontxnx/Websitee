2d605c2c6e38e8faa52945935fb65db886cf350f
Mark Luis <111257842+mlabnerdy@users.noreply.github.com>
2024-12-20 00:02:45 +0800

    Update connect.php

--- login/AdminSide/connect.php
+++ login/AdminSide/connect.php
@@ -1,9 +1,15 @@
 <?php
 
-$host="localhost";
+/*$host="localhost";
 $user="root";
 $pass="";
-$db="login";
+$db="login";*/
+
+$host="localhost";
+$user="u143688490_lou";
+$pass="Fujwiara000!";
+$db="u143688490_websiteee"; 
+
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
