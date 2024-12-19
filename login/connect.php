a1f245357be7884191eaa244b90b809ac331477e
Mark Luis <111257842+mlabnerdy@users.noreply.github.com>
2024-12-19 23:38:49 +0800

    Update connect.php

--- login/connect.php
+++ login/connect.php
@@ -1,14 +1,14 @@
 <?php
 
-$host="localhost";
+/*$host="localhost";
 $user="root";
 $pass="";
-$db="login"; 
+$db="login"; */
 
-/* $host="localhost";
+$host="localhost";
 $user="u143688490_lou";
 $pass="Fujwiara000!";
-$db="u143688490_websiteee"; */
+$db="u143688490_websiteee"; 
 
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
